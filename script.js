dfc34de490132411ea2db05cdd3f988daf3140b7
JavierUnknos <59628542+JavierUnknos@users.noreply.github.com>
2020-12-14 19:46:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -156,4 +156,109 @@ document.fonts.ready.then(function () {
   var scope = blotter.forText(text);
   scope.appendTo(container);
 
+  container = document.getElementById("2logo");
+
+  var text = new Blotter.Text("© MWARE 2021", {
+      family: "'universe', Helvetica",
+      size: 97,
+      fill: "#000000",
+      paddingLeft: 0,
+      paddingRight: 0,
+      paddingTop: 0,
+      paddingBottom: 0,
+      marginTop: 0,
+      marginBottom: 0,
+  });
+
+  var material = new Blotter.FliesMaterial();
+  material.uniforms.uPointCellWidth.value = 0.03;
+  material.uniforms.uPointRadius.value = 1.0;
+  material.uniforms.uSpeed.value = 2;
+
+  var blotter = new Blotter(material, {
+      texts: text
+  });
+
+  var scope = blotter.forText(text);
+  scope.appendTo(container);
+
+  container = document.getElementById("2line1");
+
+  var text = new Blotter.Text("AESTHETIC E-COMMERCE WITH A", {
+      family: "'garamond_narrow'",
+      size: 105,
+      fill: "#000000",
+      paddingLeft: 20,
+      paddingRight: 20,
+      paddingTop: 0,
+      paddingBottom: 0,
+      marginTop: 0,
+      marginBottom: 0,
+  });
+
+  var material = new Blotter.FliesMaterial();
+  material.uniforms.uPointCellWidth.value = 0.03;
+  material.uniforms.uPointRadius.value = 1.0;
+  material.uniforms.uSpeed.value = 2;
+
+  var blotter = new Blotter(material, {
+      texts: text
+  });
+
+  var scope = blotter.forText(text);
+  scope.appendTo(container);
+
+  container = document.getElementById("2line2");
+
+  var text = new Blotter.Text("NARRATIVE PRODUCED IN MADRID", {
+      family: "'garamond_narrow'",
+      size: 105,
+      fill: "#000000",
+      paddingLeft: 20,
+      paddingRight: 20,
+      paddingTop: 0,
+      paddingBottom: 0,
+      marginTop: 0,
+      marginBottom: 0,
+  });
+
+  var material = new Blotter.FliesMaterial();
+  material.uniforms.uPointCellWidth.value = 0.03;
+  material.uniforms.uPointRadius.value = 1.0;
+  material.uniforms.uSpeed.value = 2;
+
+  var blotter = new Blotter(material, {
+      texts: text
+  });
+
+  var scope = blotter.forText(text);
+  scope.appendTo(container);
+
+  container = document.getElementById("2line3");
+
+  var text = new Blotter.Text("AND CONSUMED AROUND THE WORLD", {
+      family: "'garamond_narrow'",
+      size: 105,
+      fill: "#000000",
+      paddingLeft: 20,
+      paddingRight: 20,
+      paddingTop: 0,
+      paddingBottom: 0,
+      marginTop: 0,
+      marginBottom: 0,
+  });
+
+  var material = new Blotter.FliesMaterial();
+  material.uniforms.uPointCellWidth.value = 0.03;
+  material.uniforms.uPointRadius.value = 1.0;
+  material.uniforms.uSpeed.value = 2;
+
+  var blotter = new Blotter(material, {
+      texts: text
+  });
+
+  var scope = blotter.forText(text);
+  scope.appendTo(container);
+
+
 }.bind(this));
